1ce6ae3ddcbdfc1943df35ee0987fcac3853ed20
MrSniper <viafaraj0@gmail.com>
2021-10-31 18:54:37 -0500

    Boss

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://www.facebook.com/profile.php?id=100025641090829">David Sanchez</a>
+    </div>
+    <div>
+      <a href="https://www.instagram.com/viafara_david/">Sigueme en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
